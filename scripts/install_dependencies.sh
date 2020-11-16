3d200efff96a76b43cb0e1a712cb8c4989c3ea19
kalyan kalapala <58908209+kalyankalapala@users.noreply.github.com>
2020-11-16 14:25:18 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,4 +4,6 @@ sudo yum install -y nodejs
 sudo yum install -y httpd
 cd /home/code-build/shark-nodejs
 sudo npm install pm2 -g
-sudo pm2 stop app.js
+sudo pm2 start app.js
+sudo npm install
+sudo npm start
