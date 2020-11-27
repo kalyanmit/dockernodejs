80a4cad4c12f0ea7336d4c94a158a980ea1f48fc
kalyankalapala <kalyankalapala24@gmail.com>
2020-11-27 10:46:52 +0530

    docker-compsoe.yml

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,3 +1,17 @@
 ### before ###
 #!/bin/bash
+
+#### install docker ###
 sudo yum install -y docker
+sudo systemctl enable docker
+sudo systemctl restart docker
+
+### install docker-compose ####
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
+#### docker-compose up ####
+cd /tmp/docker-git
+sudo docker-compose up -d
