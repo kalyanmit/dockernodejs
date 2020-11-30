753c30aea5771258e410f5c563cf3d08c2a1bda4
kalyan kalapala <58908209+kalyankalapala@users.noreply.github.com>
2020-11-30 12:41:13 +0530

    Update before.sh

--- scripts/before.sh
+++ scripts/before.sh
@@ -8,3 +8,10 @@ sudo systemctl enable nginx
 sudo systemctl restart nginx
 
 sudo rm -rf /tmp/dockernodejs
+
+### restrict the user access to run docker and to limit the access of file permissions ####
+### nano /etc/sudoers ---> add all to the new user ----> this gives the access to the user to run the docker application 
+
+## (or)
+
+### useradd user ----> usermod -a -G docker user --->  This also enables the user to run the docker containers and all the docker processes  
